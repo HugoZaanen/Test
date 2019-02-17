ba8410ea0da6a37097e9b06cca4e847e94e29748
Huego <hugozaanen@hotmail.com>
2019-02-17 15:58:03 +0100

    Add project files.

---
+++ City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class City
+    {
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+        public string Display => Name + " - " + Country;
+
+        public City(string name, string country)
+        {
+            Name = name;
+            Country = country;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Test
+{
+    class Program
+    {
+        static string[] x = { "is", "valid", "right" };
+        static string valid = "IsValid"; 
+
+        static void Main(string[] args)
+        {                                
+            Console.WriteLine(isValid(x,valid));
+            Console.ReadLine();
+        }
+
+
+        private static bool isValid(string[] words, string validation)
+        {           
+            int k = 0;
+
+            for(int i = 0; i < validation.Length;i++)
+            {
+                if(validation[i].ToString() == validation[i].ToString().ToUpper())
+                {                   
+                    string check = validation[i].ToString().ToLower();
+                    
+                    while(i < validation.Length - 1  && validation[i + 1].ToString() != validation[i + 1].ToString().ToUpper())
+                    {
+                        check += validation[i + 1].ToString();
+                        i++;
+                    }
+
+                    foreach(string word in words)
+                    {
+                        if(word == check)
+                        {
+                            k++;
+                        }
+                    }
+
+                    if(k == 0)
+                    {
+                        return false;
+                    }
+                }              
+            }
+
+            if (k == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }    
+}
